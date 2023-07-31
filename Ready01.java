f585a3f851fbf00c3cc3fd89c2f449a238c30c2e
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-07-31 10:06:30 +0000

    関数の意味

--- Ready01.java
+++ Ready01.java
@@ -1,5 +1,13 @@
 public class Ready01 {
   public static void main(String[] args){
-    System.out.println("Hello world!"); //aaa
+    /*
+    public...クラス内関数が外部からアクセス可能 →main関数はどこからでも利用できる
+    static...データ型であるクラス内の関数を利用可能に 
+              →実体として形が作られていないクラスの関数を使うため
+    void...戻り値 →main関数は戻り値を必要としない
+    main...関数名
+    (String[] args)...引数・String型の文字列を扱う配列
+    */
+    System.out.println("Hello world!");
   }
 }
