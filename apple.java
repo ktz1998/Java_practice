d6ffcaaaba221fc8cc601a33dc96ee0b3d3114ba
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-07 14:54:21 +0000

    変数宣言/初期化

---
+++ apple.java
@@ -0,0 +1,10 @@
+public class apple{
+  public static void main (String[] args) {
+    int apple; //int型のメモリスペース(4バイト)を確保しappleと名付ける
+    apple = 10; //メモリスペースに代入
+    System.out.println(apple);
+    
+    int num = 15; //変数の初期化(宣言と代入を同時にやる)
+    System.out.println(num);
+  }
+}
