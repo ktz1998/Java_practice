2f0653ffa4aafa8f25a5faba3bd86e80687675ba
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-07 14:40:18 +0000

    フォルダ分け/変数

---
+++ 変数.java
@@ -0,0 +1,9 @@
+public class 変数{
+  public static void main (String[] args) {
+    System.out.println(3 * 3 * 3.14);
+    //変数を使用
+    int r;
+    r = 3;
+    System.out.println(r * r * 3.14);
+  }
+}
