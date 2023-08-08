f57ead9a1722b34335c0d1000c80bd48f2d250cc
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-08 10:09:48 +0000

    インクリメント/デクリメント

---
+++ Increment.java
@@ -0,0 +1,13 @@
+public class Increment{
+  public static void main (String[] args) {
+    int x = 10;
+    int y = 10;
+    //インクリメント
+    x++;
+    //デクリメント
+    y--;
+    
+    System.out.println("xの値：" + x);
+    System.out.println("yの値：" + y);
+  }
+} 
