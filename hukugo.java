b6f0dd18c424e143508076827fc7812dc6cac8f0
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-08 10:05:11 +0000

    複合代入演算子

---
+++ hukugo.java
@@ -0,0 +1,10 @@
+public class hukugo{
+  public static void main (String[] args) {
+    int apple = 10;
+    apple += 5; //複合代入演算子
+    apple -= 5;
+    apple *= 5;
+    apple /= 5;
+    System.out.println(apple);
+  }
+} 
