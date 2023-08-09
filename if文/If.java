26acbb314b7b60cd455af4900aa670b881f0a56e
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-09 14:35:10 +0000

    関係演算子

--- if文/If.java
+++ if文/If.java
@@ -7,10 +7,16 @@ public class If{
       System.out.println("実行されません");
     
     //等値演算子
-    int num = 9;
+    int num = 100;
     if(num == 10)
       System.out.println("10と同じ");
     if(num != 10)
       System.out.println("10と異なる");
+    //関係演算子
+    if(num > 10)
+      System.out.println("10より大きい");
+    if(num < 10)
+      System.out.println("10より小さい");
+    
   }
 }
