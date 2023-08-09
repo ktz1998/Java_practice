982ad6a4ce63b2a5e64cfd8e1c2c6b4ec0407b3a
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-09 14:26:53 +0000

    if文

---
+++ if文/If.java
@@ -0,0 +1,9 @@
+public class If{
+  public static void main (String[] args) {
+    //条件分岐
+    if (true)
+      System.out.println("実行されます");
+    if (false)
+      System.out.println("実行されません");
+  }
+}
