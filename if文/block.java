2a06922a636d12c4605ed7e3010014009d46f191
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-14 14:30:21 +0000

    ブロック構文

---
+++ if文/block.java
@@ -0,0 +1,10 @@
+public class block{
+  public static void main(String[] args){
+    int num = 20;
+    //ブロック構文：ひとつのif文で複数の処理を実行できる
+    if(num != 10){  
+      System.out.println("10と異なる");
+      System.out.println("もう一度入力してください");
+    }
+  }
+}
