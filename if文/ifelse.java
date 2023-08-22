0717be33d59babfcee6a68abacebd0509d1d043d
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-22 14:41:15 +0000

    if else文

---
+++ if文/ifelse.java
@@ -0,0 +1,10 @@
+public class ifelse{
+  public static void main (String[] args) {
+    //数値が10の時とそうでない時
+    int num = 10;
+    if(num == 10)
+      System.out.println("10と同じ");
+    else
+      System.out.println("10と同じではない");
+  }
+}
