352acdf367258adc99d3e2c9a7200bc59fec070d
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-22 14:47:48 +0000

    if else文（3つの条件比較追加

--- if文/ifelse.java
+++ if文/ifelse.java
@@ -6,5 +6,16 @@ public class ifelse{
       System.out.println("10と同じ");
     else
       System.out.println("10と同じではない");
+    
+    //3つの条件の比較
+    //3歳以下、3～20歳、20歳以上で条件を分ける
+    int year = 30;
+    if(year <= 3) //3歳以下
+      System.out.println("0円です");
+    else if (year <= 20) //3歳以下除く20歳以下（つまり3～20歳）
+      System.out.println("1000円です");
+    else //20歳以上
+      System.out.println("2000円です");
+    
   }
 }
