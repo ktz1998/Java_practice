c61ce46a8af0f56a9892cb7f91e8904aa4b68fcf
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-23 03:07:00 +0000

    for文

---
+++ 繰り返し/For.java
@@ -0,0 +1,9 @@
+public class For{
+  public static void main (String[] args) {
+  //for文：回数を指定した繰り返し
+    int i; //繰り返した回数を記憶するために宣言
+    for (i = 0; i < 3; i++){ //（記憶用変数を初期化；条件式；繰り返しする処理）
+      System.out.println(i + "回目メッセージ");
+    } 
+  }
+}
