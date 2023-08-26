581f5f3e0ff293a95d6ec5bbeb156c5fefa0c46e
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-26 14:42:05 +0000

    関数の作成と呼び出し

---
+++ 関数/hello.java
@@ -0,0 +1,11 @@
+public class hello{
+    public static void hello(){  //hello関数の作成
+        System.out.println("Hello World");
+    }
+   
+    public static void main (String[] args) {
+        hello();  //関数の呼び出し
+        hello();
+        hello();
+    }
+} 
