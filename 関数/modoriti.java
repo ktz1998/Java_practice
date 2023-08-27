e27efbe9b07b2b7ec88b2c03e6ac3cb03185bf24
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-08-27 15:08:02 +0000

    戻り値の書き方

--- 関数/modoriti.java
+++ 関数/modoriti.java
@@ -1,7 +1,14 @@
 public class modoriti{
-    public static int keisan(int x){
+    public static int keisan(int x){  //戻り値でintを指定
+        int goukei = x * 200;  //goukeiはappleの合計金額
+        return goukei;  //戻り値としてkeisan関数に返却
+    }
     public static void main (String[] args) {
-        int apple = 10;
-        keisan(apple);
+        int apple = 100;
+        int result = keisan(apple);  //合計金額をresultに
+        if (result > 3000)
+            System.out.println("予算オーバー");
+        else
+            System.out.println("予算以内で購入可");
     }
 } 
